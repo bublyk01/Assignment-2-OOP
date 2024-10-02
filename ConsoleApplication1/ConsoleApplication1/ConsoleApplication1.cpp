a9c67b31c0de5a39a03f1657b04396e451fc93f6
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-10-02 20:57:23 +0300

    Board was created

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+const int BOARD_WIDTH = 80;
+const int BOARD_HEIGHT = 25;
+
+struct Board {
+    std::vector<std::vector<char>> grid;
+
+    Board() : grid(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH, ' ')) {}
+
+    void print() {
+        for (auto& row : grid) {
+            for (char c : row) {
+                std::cout << c;
+            }
+            std::cout << "\n";
+        }
+    }
+};
+
+int main() {
+    Board board;
+    std::string command;
+
+    while (true) {
+        std::cout << "> ";
+        std::cin >> command;
+
+        if (command == "draw") {
+            board.print();
+        }
+        else if (command == "exit") {
+            break;
+        }
+    }
+
+    return 0;
+}
