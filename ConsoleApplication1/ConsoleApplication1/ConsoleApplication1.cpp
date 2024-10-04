eeb52699412cfa95c99cd2a81df6dcd58bc0b931
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-10-04 13:59:34 +0300

    Fix to the circle was added

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,10 +5,11 @@
 
 const int BOARD_WIDTH = 80;
 const int BOARD_HEIGHT = 80;
+const int FIGURE_SCALE = 2.0;
 
 struct Board {
     std::vector<std::vector<char>> grid;
-    Board() : grid(25, std::vector<char>(80, ' ')) {}
+    Board() : grid(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH, ' ')) {}
 
     void print() {
         for (auto& row : grid) {
@@ -20,41 +21,13 @@ struct Board {
     }
 
     void clear() {
-        grid.assign(25, std::vector<char>(80, ' '));
+        grid.assign(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH, ' '));
     }
 };
 
 struct Shape {
     virtual void draw(Board& board, int x, int y) = 0;
     virtual ~Shape() = default;
-};
-
-struct Triangle : public Shape {
-    int height;
-
-    Triangle(int h) : height(h) {}
-
-    void draw(Board& board, int x, int y) override {
-        if (height <= 0) return;
-        for (int i = 0; i < height; ++i) {
-            int left = x - i;
-            int right = x + i;
-            int posY = y + i;
-
-            if (posY < 25) {
-                if (left >= 0 && left < 80)
-                    board.grid[posY][left] = '*';
-                if (right >= 0 && right < 80 && left != right)
-                    board.grid[posY][right] = '*';
-            }
-        }
-        for (int j = 0; j < 2 * height - 1; ++j) {
-            int baseX = x - height + 1 + j;
-            int baseY = y + height - 1;
-            if (baseX >= 0 && baseX < 80 && baseY < 25)
-                board.grid[baseY][baseX] = '*';
-        }
-    }
 };
 
 struct Circle : public Shape {
@@ -64,12 +37,15 @@ struct Circle : public Shape {
 
     void draw(Board& board, int X, int Y) override {
         if (radius <= 0) return;
+
         for (int y = -radius; y <= radius; ++y) {
             for (int x = -radius; x <= radius; ++x) {
-                if (std::abs(x * x + y * y - radius * radius) <= radius) {
+                float correctY = y * FIGURE_SCALE;
+                float distance = sqrt(x * x + correctY * correctY);
+                if (std::abs(distance - radius) <= 0.5) {
                     int drawnX = X + x;
                     int drawnY = Y + y;
-                    if (drawnX >= 0 && drawnX < 80 && drawnY >= 0 && drawnY < 25) {
+                    if (drawnX >= 0 && drawnX < BOARD_WIDTH && drawnY >= 0 && drawnY < BOARD_HEIGHT) {
                         board.grid[drawnY][drawnX] = '*';
                     }
                 }
@@ -88,14 +64,6 @@ int main() {
 
         if (command == "draw") {
             board.print();
-        }
-        else if (command == "triangle") {
-            int x, y, height;
-            std::cout << "Enter the location of the triangle, and its height: ";
-            std::cin >> x >> y >> height;
-
-            Triangle triangle(height);
-            triangle.draw(board, x, y);
         }
         else if (command == "circle") {
             int x, y, radius;
